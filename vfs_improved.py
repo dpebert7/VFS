615c7a8a4ff7a3fb252c3134f387db1d493f24c2
David E <dpebert7@users.noreply.github.com>
2023-04-26 23:30:26 +0200

    Add files via upload

---
+++ vfs_improved.py
@@ -0,0 +1,55 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+NAME_BOX_XPATH = "/html/body/form/div[3]/table/tbody/tr[2]/td/div/div[2]/table/tbody/tr[1]/td[2]/table/tbody/tr/td/table/tbody/tr[3]/td[2]/input"
+AURN_BOX_XPATH = "/html/body/form/div[3]/table/tbody/tr[2]/td/div/div[2]/table/tbody/tr[1]/td[2]/table/tbody/tr/td/table/tbody/tr[2]/td[2]/input"
+SUBMIT_BUTTON_XPATH = "/html/body/form/div[3]/table/tbody/tr[2]/td/div/div[2]/table/tbody/tr[1]/td[2]/table/tbody/tr/td/table/tbody/tr[4]/td[2]/input[1]"
+TEXT_FIELD_RESULT_XPATH = "/html/body/form/div[3]/table/tbody/tr[2]/td/div/div[2]/table/tbody/tr[1]/td[2]/table/tbody/tr/td/table/tbody/tr[5]/td/span[2]"
+#https://machinelearningmastery.com/a-gentle-introduction-to-unit-testing-in-python/
+
+
+class VFS:
+    def __init__(self, surname, aurn):
+        self.surname = surname
+        self.aurn = aurn
+
+    def get_status(self):
+        #driver = webdriver.Chrome(executable_path='./chromedriver.exe') # This gives deprecation warning
+        s = Service('./chromedriver.exe')
+        driver = webdriver.Chrome(service=s)
+        driver.get("https://www.vfsvisaonline.com/DHAOnlineTracking/OnlineTracking.aspx")
+        time.sleep(2)
+        driver.find_element(by=By.XPATH, value=NAME_BOX_XPATH).send_keys(self.surname)
+        driver.find_element(by=By.XPATH, value=AURN_BOX_XPATH).send_keys(self.aurn)
+        driver.find_element(by=By.XPATH, value=SUBMIT_BUTTON_XPATH).click()
+        time.sleep(2)
+        current_status = driver.find_element(by=By.XPATH, value=TEXT_FIELD_RESULT_XPATH).text
+        driver.quit()
+        return(current_status)
+
+
+class TestVFS(unittest.TestCase):
+    def test_trp_1(self):
+        vfs_trp_1 = VFS('Ebert', 'TRRzzzzzzzzz')
+        self.assertEqual(vfs_trp_1.get_status(), 'Application for zzzzzzz has been received at DHA on 26 Jan 2022.', 'Unexpected status for Bram TRP')
+
+    def test_trp_2(self):
+        vfs_trp_2 = VFS('Ebert', 'TRRzzzzzzzzz')
+        self.assertEqual(vfs_trp_2.get_status(), 'Application for zzzzzzz has been received at DHA on 27 Sep 2021.', 'Unexpected status for David TRP')
+
+    def test_trp_3(self):
+        vfs_trp_3 = VFS('Ebert', 'TRRzzzzzzzzz')
+        self.assertEqual(vfs_trp_3.get_status(), 'Application for zzzzzzz has been received at DHA on zzzzzzzz.', 'Unexpected status for Bram PRP')
+
+
+unittest.main()
+
+
+
+
+
+
+    
---
+++ vfs_orig.py
@@ -0,0 +1,68 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+NAME_BOX_XPATH = "/html/body/form/div[3]/table/tbody/tr[2]/td/div/div[2]/table/tbody/tr[1]/td[2]/table/tbody/tr/td/table/tbody/tr[3]/td[2]/input"
+AURN_BOX_XPATH = "/html/body/form/div[3]/table/tbody/tr[2]/td/div/div[2]/table/tbody/tr[1]/td[2]/table/tbody/tr/td/table/tbody/tr[2]/td[2]/input"
+SUBMIT_BUTTON_XPATH = "/html/body/form/div[3]/table/tbody/tr[2]/td/div/div[2]/table/tbody/tr[1]/td[2]/table/tbody/tr/td/table/tbody/tr[4]/td[2]/input[1]"
+TEXT_FIELD_RESULT_XPATH = "/html/body/form/div[3]/table/tbody/tr[2]/td/div/div[2]/table/tbody/tr[1]/td[2]/table/tbody/tr/td/table/tbody/tr[5]/td/span[2]"
+
+
+
+driver = webdriver.Chrome(executable_path='./chromedriver.exe')
+driver.get("https://www.vfsvisaonline.com/DHAOnlineTracking/OnlineTracking.aspx")
+
+result_text_fields = []
+usual_result = [
+    'Application for foo has been received at DHA on date1.',
+    'Application for bar has been received at DHA on date2.',
+    'Application for baz has been received at DHA on date3.',
+]
+
+
+
+driver.find_element(by=By.XPATH, value=NAME_BOX_XPATH).send_keys("foo")
+driver.find_element(by=By.XPATH, value=AURN_BOX_XPATH).send_keys("TRRbazbar1")
+driver.find_element(by=By.XPATH, value=SUBMIT_BUTTON_XPATH).click()
+time.sleep(0.5)
+result_text_fields.append(driver.find_element(by=By.XPATH, value=TEXT_FIELD_RESULT_XPATH).text)
+test_result = 'Application for foo has been received at DHA on date1.'
+time.sleep(0.25)
+
+
+driver.find_element(by=By.XPATH, value=AURN_BOX_XPATH).clear()
+driver.find_element(by=By.XPATH, value=AURN_BOX_XPATH).send_keys("TRRbazbar2")
+driver.find_element(by=By.XPATH, value=SUBMIT_BUTTON_XPATH).click()
+time.sleep(0.5)
+result_text_fields.append(driver.find_element(by=By.XPATH, value=TEXT_FIELD_RESULT_XPATH).text)
+test_result = 'Application for bar has been received at DHA on date2.'
+time.sleep(0.5)
+
+
+driver.find_element(by=By.XPATH, value=AURN_BOX_XPATH).clear()
+driver.find_element(by=By.XPATH, value=AURN_BOX_XPATH).send_keys("TRRbazbar3")
+driver.find_element(by=By.XPATH, value=SUBMIT_BUTTON_XPATH).click()
+time.sleep(0.5)
+result_text_fields.append(driver.find_element(by=By.XPATH, value=TEXT_FIELD_RESULT_XPATH).text)
+test_result = 'Application for baz has been received at DHA on date3.'
+time.sleep(0.5)
+
+driver.quit()
+
+
+#print(usual_result, result_text_fields)
+if usual_result == result_text_fields:
+    print('Nothing new... keep waiting... :|')
+else:
+    print('Unusual result. Check again to see if a Visa has arrived.')
+    print('Expected result: ', usual_result)
+    print('Actual result: ', result_text_fields)
+
+
+
+
+
+
+
+    
